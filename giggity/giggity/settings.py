142c038064b49a8149aefd0a4ed234d8f5effbd2
Varad Joshi <varad.gcs@gmail.com>
2023-10-25 19:00:07 +0530

    Move to using secrets.py and add template for secrets.py

--- giggity/giggity/settings.py
+++ giggity/giggity/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from . import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gk=1ys+0f_pzoj^gw0=6e=%n6giii_jr5e3n1njf(k$fx^54o$'
+SECRET_KEY = secrets.KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
