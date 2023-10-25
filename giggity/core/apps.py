65f1507432aa1b8a702a3ad6617ff09acda81558
Varad Joshi <varad.gcs@gmail.com>
2023-10-25 19:53:24 +0530

    Setup signals for populating user interests and creating freelancer profile whenever a new user profile is created

--- giggity/core/apps.py
+++ giggity/core/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
-
+from allauth.account.signals import user_signed_up
+from django.db.models.signals import connect  # Corrected import statement
+from core.user_handlers import create_freelancer_profile, populate_user_interests
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        user_signed_up.connect(create_freelancer_profile, sender=self)
+        user_signed_up.connect(populate_user_interests, sender=self)
