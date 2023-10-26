9182fad8fa3d445fff06917809384b7b0bb8005e
Varad Joshi <varad.gcs@gmail.com>
2023-10-27 02:55:32 +0530

    Update recommendation engine with top charts

--- giggity/recommendation_engine.py
+++ giggity/recommendation_engine.py
@@ -5,27 +5,22 @@ import numpy as np
 con = None
 
 def connect_db(db_name):
-    # Tries to connect 50 times after that stops script execution
-    for x in range(50):
-        try:
-            connection = sqlite3.connect(db_name)
-            # Connection success! Break out of for loop as well as function and return connection to main
-            return connection
-        except sqlite3.Error as e:
-            # Display error and attempt number in console
-            print("error"+e)
-    # Could not connect 50 times so stop execution of the script
+    try:
+        connection = sqlite3.connect(db_name)
+        return connection
+    except sqlite3.Error as e:
+        print("error"+str(e))
     sys.exit(1)
 
 def fetch_users():
-    query = 'SELECT id from core_userprofile'
+    query = 'SELECT id from core_userprofile;'
     cursor = con.cursor()
     cursor.execute(query)
     results = cursor.fetchall()
     return results
 
 def fetch_posts():
-    query = 'SELECT post_id from core_post'
+    query = 'SELECT post_id from core_post;'
     cursor = con.cursor()
     cursor.execute(query)
     results = cursor.fetchall()
@@ -96,19 +91,22 @@ def cosine_similarity(dict1, dict2):
     dot_product = np.dot(vector1, vector2)
     magnitude1 = np.linalg.norm(vector1)
     magnitude2 = np.linalg.norm(vector2)
+    if dot_product==0:
+        return 0
     similarity = dot_product / (magnitude1 * magnitude2)
+    print(str(similarity))
     return similarity
 
 def recommend(userid, postid, similarity):
     cacherecommendations(userid, postid)
     print("Generated Recommendation")
-    query = 'INSERT into core_recommendations (post_id, user_id, score, visited) values ('+str(postid)+','+str(userid)+','+str(similarity)+',"False")'
+    query = 'INSERT into core_recommendations (post_id, user_id, score, visited) values ('+str(postid)+','+str(userid)+','+str(similarity)+',"False");'
     cursor = con.cursor()
     cursor.execute(query)
     con.commit()
 
 def cacheinteractions():
-    query = 'DELETE FROM core_interaction'
+    query = 'DELETE FROM core_interaction;'
     cursor = con.cursor()
     cursor.execute(query)
     con.commit()
@@ -119,49 +117,111 @@ def cacherecommendations(userid, postid):
     cursor.execute(query)
     con.commit()
 
+def generate_recommendations(users, posts):
+    for r in users:
+        user = r[0]
+        # Fetching user interests
+        user_interests_data = fetch_userinterests(user)
+        if user_interests_data:
+            user_interests = {}
+            for row in user_interests_data:
+                tag = row[0]
+                score = row[1]
+                user_interests[tag] = score
+            # Start collecting user interactions
+            user_interactions_data = fetch_interactionscores(user)
+            if user_interactions_data:
+                user_interactions = {}
+                print("No interactions detected for user")
+                for row in user_interactions_data:
+                    tag = row[0]
+                    score = row[1]
+                    user_interactions[tag] = score
+                # Normalise interaction scores to 10
+                user_interactions = normalise(user_interactions)
+                user_scores = generatescores(user_interests, user_interactions)
+                updatescore(user_scores, user)
+    # Iterate over posts
+    for r in posts:
+        post = r[0]
+        # Fetch post tags
+        post_tags = {}
+        for row in fetch_posttags(post):
+            tag = row[0]
+            score = row[1]
+            post_tags[tag] = score
+        # Iterate over users
+        for row in users:
+            user = row[0]
+            user_interests = {}
+            for row in fetch_userinterests(user):
+                tag = row[0]
+                score = row[1]
+                user_interests[tag] = score 
+            similarity = cosine_similarity(user_interests, post_tags)
+            if similarity > 0.5:
+                recommend(user, post, similarity)
+    cacheinteractions()
+
+def generate_latest_top():
+    cursor = con.cursor()
+    cursor.execute("""
+        SELECT post_id, COUNT(post_id) as post_count
+        FROM core_interaction
+        GROUP BY post_id
+        ORDER BY post_count DESC
+        LIMIT 5
+    """)
+    latest_posts = cursor.fetchall()
+    for post_id, _ in latest_posts:
+        save_top(post_id, 'Latest')
+
+def generate_top():
+    cursor = con.cursor()
+    cursor.execute("""
+        SELECT post_id, COUNT(post_id) as post_count
+        FROM core_logs
+        GROUP BY post_id
+        ORDER BY post_count DESC
+        LIMIT 5
+    """)
+    top_posts = cursor.fetchall()
+    for post_id, _ in top_posts:
+        save_top(post_id, 'Top')
+
+def generate_grossing():
+    cursor = con.cursor()
+    cursor.execute("""
+        SELECT post_id, COUNT(post_id) as post_count
+        FROM core_recommendations
+        GROUP BY post_id
+        ORDER BY post_count DESC
+        LIMIT 5
+    """)
+    grossing_posts = cursor.fetchall()
+    for post_id, _ in grossing_posts:
+        save_top(post_id, 'Grossing')
+
+def save_top(post_id, type):
+    print("generated top chart"+" PostID:"+str(post_id)+" Type:"+str(type))
+    query = 'INSERT into core_topcharts (type, post_id) values (?, ?);'
+    cursor = con.cursor()
+    cursor.execute(query, (type, post_id))
+    con.commit()
+
+def cachetop():
+    query = 'DELETE FROM core_topcharts;'
+    cursor = con.cursor()
+    cursor.execute(query)
+    con.commit()
+
+#Main execution
 con = connect_db('db.sqlite3')
 users = fetch_users()
 posts = fetch_posts()
-# Iterate over users
-for r in users:
-    user = r[0]
-    # Fetching user interests
-    if fetch_userinterests(user):
-        user_interests = {}
-        for row in fetch_userinterests(user):
-            tag = row[0]
-            score = row[1]
-            user_interests[tag] = score
-        # Start collecting user interactions
-        if fetch_interactionscores(user):
-            user_interactions = {}
-            print("No interactions detected for user")
-            for row in fetch_interactionscores(user):
-                tag = row[0]
-                score = row[1]
-                user_interactions[tag] = score
-            # Normalise interaction scores to 10
-            user_interactions = normalise(user_interactions)
-            user_scores = generatescores(user_interests, user_interactions)
-            updatescore(user_scores, user)
-# Iterate over posts
-for r in posts:
-    post = r[0]
-    # Fetch post tags
-    post_tags = {}
-    for row in fetch_posttags(post):
-        tag = row[0]
-        score = row[1]
-        post_tags[tag] = score
-    # Iterate over users
-    for row in users:
-        user = row[0]
-        user_interests = {}
-        for row in fetch_userinterests(user):
-            tag = row[0]
-            score = row[1]
-            user_interests[tag] = score 
-        similarity = cosine_similarity(user_interests, post_tags)
-        if similarity > 0.5:
-            recommend(user, post, similarity)
-cacheinteractions()
+cachetop()
+generate_latest_top()
+generate_top()
+generate_recommendations(users, posts)
+generate_grossing()
+con.close()
